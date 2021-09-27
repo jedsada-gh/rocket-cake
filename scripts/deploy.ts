f2cdb67af0e5beee5829e1e2d472e272ffd58b1a
Jedsada Tiwongvorakul <jedsada.t@20scoops.net>
2021-09-27 13:45:08 +0700

    chore: add folder deployed

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+task("deploy", "Rocket Contract", async (_, hre) => {
+  const contracts = { rocketToken: "",  iterableMapping: ""};
+  const IterableMapping = await hre.ethers.getContractFactory("IterableMapping");
+  const iterableMapping = await IterableMapping.deploy();
+
+  const RocketCake = await hre.ethers.getContractFactory("ROCKETCAKE", {
+    libraries: {
+      IterableMapping: iterableMapping.address,
+    },
+  });
+
+  const rocketCake = await RocketCake.deploy();
+
+  await rocketCake.deployed();
+
+  contracts.rocketToken = rocketCake.address;
+  contracts.iterableMapping = iterableMapping.address
+  console.log("Rocket deployed to ✅");
+
+  const resultPath = path.join(__dirname, `./constants/${hre.network.name}.json`);
+  fs.writeFileSync(resultPath, JSON.stringify(contracts, null, 2));
+
+  console.log(JSON.stringify(contracts, null, 2));
+});
